ebcf12042ac63399d203eb3e5b3e2dec87f0cf80
yussems <yussems@gmail.com>
2022-02-06 14:36:35 +0300

    first email created

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgMfFgp-yUS-Ux1pAftFI75NnLNyxt-gk",
+  authDomain: "liberyus-afb42.firebaseapp.com",
+  projectId: "liberyus-afb42",
+  storageBucket: "liberyus-afb42.appspot.com",
+  messagingSenderId: "859416389549",
+  appId: "1:859416389549:web:8331a1a6d5f52adf132324"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
