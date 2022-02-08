d304c43f6240f6359438013d2028a2b22e60457c
yussems <yussems@gmail.com>
2022-02-08 16:27:21 +0300

    color logic fixed, warning message fixed, some firestore delete problem

--- component/Cards.js
+++ component/Cards.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
-import { Card, message } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Card, message, Space } from "antd";
 import styles from "../styles/Card.module.css";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { deleteDomain } from "../utils/firestore";
 
 function Cards({ data }) {
-  const [color, setColor] = useState("");
+  const idRef = useRef(null);
 
-  return (
-    <div className={styles.wrapper}>
-      {data.map((element) => {
-        const { days, domaninName, id, whereToTake } = element;
-        let color = "";
-        let warning = "";
+  const warningEffect = (days,domaninName) => {
+    if (days < 15) {
+      message.warning(`${domaninName} süresi az 15 günden az kalmıştır`);
+    }
+  };
+
+  let color = "";
+  function colorState(days) {
+        
         if (days < 15) {
           color = "red";
         }
@@ -20,11 +25,23 @@ function Cards({ data }) {
         if (days > 60) {
           color = "green";
         }
-        if (days < 15) {
-           message.warning(`${domaninName} süresi az 15 günden az kalmıştır`);
-        }
+  }
+  // useEffect(() => {
+  //   warningEffect(days)
+  // }, []);
+
+  console.log();
+  const domainDelete = () => {
+    deleteDomain(idRef.current.id);
+  };
+  return (
+    <div className={styles.wrapper}>
+      {data.map((element) => {
+        const { days, domaninName, id, whereToTake } = element;
+        colorState(days)
+        warningEffect(days,domaninName)
         return (
-          <div key={id} className={styles.card}>
+          <div key={id} ref={idRef} id={id} className={styles.card}>
             <Card
               headStyle={{ backgroundColor: color }}
               title={domaninName}
@@ -41,6 +58,14 @@ function Cards({ data }) {
                 <span>Nereden alındı:</span> {whereToTake || "Veri girilmedi"}
               </p>
             </Card>
+            <Card
+              style={{ width: 500, borderTop: "none" }}
+              bodyStyle={{ display: "none" }}
+              actions={[
+                <EditOutlined key="edit" />,
+                <DeleteOutlined onClick={domainDelete} key="delete" />,
+              ]}
+            ></Card>
           </div>
         );
       })}
--- utils/firestore.js
+++ utils/firestore.js
@@ -1,20 +1,32 @@
-import { addDoc, collection, getDocs, onSnapshot } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
-const colRef = collection(db,'domains')
+const colRef = collection(db, "domains");
 
-export function addData(name,taken,days) {
-    addDoc(colRef,{
-        domaninName: name,
-        whereToTake: taken,
-        days:days
-    })
+export function addData(name, taken, days) {
+  addDoc(colRef, {
+    domaninName: name,
+    whereToTake: taken,
+    days: days,
+  });
 }
-export const  takeData = async () => {
-    let domains = []
-    const checkdata =  await getDocs(colRef)
-        checkdata.docs.forEach(res => {
-            domains.push({...res.data(),id:res.id})
-    })
-    return domains
-} 
+export const takeData = async () => {
+  let domains = [];
+  const checkdata = await getDocs(colRef);
+  checkdata.docs.forEach((res) => {
+    domains.push({ ...res.data(), id: res.id });
+  });
+  return domains;
+};
+
+export const deleteDomain = async (id) => {
+  const docref = doc(colRef, id);
+  await deleteDoc(docref);
+};
